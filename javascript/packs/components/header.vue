c7b681301d0143117f509c06517590a81a33f8b2
EC2 Default User <ec2-user@ip-172-31-24-225.ap-southeast-1.compute.internal>
2019-02-11 12:48:16 +0000

    test

---
+++ javascript/packs/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+<nav>
+  <div class="nav-wrapper">
+      <ul>
+          <li><router-link to="/">Top</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/myskills">My skills</router-link></li>
+          <li><router-link to="/works">Works</router-link></li>
+          <li><router-link to="/contacts">Contacts</router-link></li>
+        </ul>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+
+.nav-wrapper{
+    display: table;
+    margin: auto;
+    text-align: center;
+    font-family: 'Yeseva One', cursive;
+    font-size: 40px;
+}
+
+.li{
+  display: table-cell;
+  
+}
+
+
+
+</style>
---
+++ javascript/packs/components/myskills.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="card-wrapper"> 
+  <div class="cards">
+  <div class="card">
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">HTML & CSS</span>
+      <p class="card-sub">基本的な知識をProgateで学習しました。</p>
+    </div>
+  </div>
+  
+  <div class="card">
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Vue.js</span>
+      <p class="card-sub">こちらのポートフォリオサイトを作成するため学習しました。単一ファイルコンポーネントやvuerouterを使用し、SPAの実装を行いました。</p>
+    </div>
+  </div>
+  
+  <div class="card">
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Ruby</span>
+      <p class="card-sub">オリジナルの画像投稿アプリを作成する過程で、学習しました。</p>
+    </div>
+  </div>
+  
+  <div class="card">
+    
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Rubyonrails</span>
+      <p class="card-sub">画像投稿アプリ作成において、ログイン・ログアウト機能、rememberme機能、一覧表示機能等を作成しました。</p>
+    </div>
+  </div>
+  </div>
+  
+</div>
+  
+  
+</template>
+
+<style scoped>
+
+.card-wrapper{
+  padding-top: 40px;
+  display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.cards{
+  width:900px;
+}
+
+.card{
+  float:left;
+ width: 400px;
+ height: 150px;
+ margin-left:20px;
+  margin-right:10px;
+}
+
+
+.card-sub{
+  text-align: left;
+  padding:5px 5px;
+  word-break: break-all;
+}
+
+</style>
